bea16f79a57727e2d90ec6a8e32130926a88937f
Benjamin Atlas <benjamin.s.atlas@gmail.com>
2024-02-02 07:46:20 -0700

    CORS handling

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -10,6 +11,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json({ limit: process.env.REQUEST_LIMIT }));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 const programsRouter = require('./routes/programs');
 const templatesRouter = require('./routes/templates');
